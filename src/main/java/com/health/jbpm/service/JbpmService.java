7a1ecbfe8d685ef20490e336aa598f883a22b7f2
Avishek Das <avishekdas.cool@gmail.com>
2020-05-21 19:26:51 +0530

    added appointment srvc

--- src/main/java/com/health/jbpm/service/JbpmService.java
+++ src/main/java/com/health/jbpm/service/JbpmService.java
@@ -79,6 +79,7 @@ public class JbpmService implements IJbpmService {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("myurl", "http://3.7.152.131:4040/api/v1/appointments/");
 		params.put("mymethod", "PUT");
+		params.put("mycontenttype", "application/json");
 		if(apptData == null) {
 			params.put("mypatientname", "john");
 			params.put("mydoctorname", "Dr.D");
